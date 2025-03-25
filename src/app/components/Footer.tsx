0ef65c760b538f73e0a9c0cc5238ea60daf8a57d
Gabriel Cavalcante <94535032+zolppy@users.noreply.github.com>
2025-03-25 12:11:51 -0300

    fix: target blank at a tag from footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
             Powered by{" "}
             <a
                 href="https://github.com/zolppy"
+                target="_blank"
                 className="underline active:opacity-60 lg:hover:opacity-60"
             >
                 Zolppy
