f6ccd5bf0602828189d223e96baa74b7c3c2757d
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-05-04 18:12:01 -0300

    test: forminputwrapper test

--- src/app/components/FormInputWrapper/index.test.tsx
+++ src/app/components/FormInputWrapper/index.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { FormInputWrapper } from "@/app/components/FormInputWrapper";
 
 describe("FormInputWrapper Componet", function () {
-    it("", () => {});
+    // Test basic rendering and children
+    it("should render children correctly", function () {
+        const text = "Some text";
+        render(<FormInputWrapper>{text}</FormInputWrapper>);
+        expect(screen.getByText(text)).toBeInTheDocument();
+    });
 });
