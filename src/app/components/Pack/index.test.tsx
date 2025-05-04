cce18f42968a7b4b95141ea5ed0c99f03e3c01d5
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-05-04 18:13:00 -0300

    test: pack test

--- src/app/components/Pack/index.test.tsx
+++ src/app/components/Pack/index.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { Pack } from "@/app/components/Pack";
 
 describe("Pack Componet", function () {
-    it("", () => {});
+    // Test basic rendering and children
+    it("should render children correctly", function () {
+        const text = "Some text";
+        render(<Pack>{text}</Pack>);
+        expect(screen.getByText(text)).toBeInTheDocument();
+    });
 });
