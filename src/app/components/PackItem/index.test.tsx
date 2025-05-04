961219d848d01ad4fb08f911ba2cc25f7b21b98f
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-05-04 18:13:09 -0300

    test: packitem test

--- src/app/components/PackItem/index.test.tsx
+++ src/app/components/PackItem/index.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { PackItem } from "@/app/components/PackItem";
 
 describe("PackItem Componet", function () {
-    it("", () => {});
+    // Test basic rendering and children
+    it("should render children correctly", function () {
+        const text = "Some text";
+        render(<PackItem>{text}</PackItem>);
+        expect(screen.getByText(text)).toBeInTheDocument();
+    });
 });
