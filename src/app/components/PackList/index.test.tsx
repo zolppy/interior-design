9e3c23efdf9943f673eb02547e4a9a7f963121d2
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-05-04 18:13:23 -0300

    test: packlist test

--- src/app/components/PackList/index.test.tsx
+++ src/app/components/PackList/index.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { PackList } from "@/app/components/PackList";
 
 describe("PackList Componet", function () {
-    it("", () => {});
+    // Test basic rendering and children
+    it("should render children correctly", function () {
+        const text = "Some text";
+        render(<PackList>{text}</PackList>);
+        expect(screen.getByText(text)).toBeInTheDocument();
+    });
 });
