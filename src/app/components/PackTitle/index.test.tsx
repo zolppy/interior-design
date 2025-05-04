5df25d546a6cfb0fe3a74b2ee41fbe726f802f70
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-05-04 18:13:34 -0300

    test: packtitle test

--- src/app/components/PackTitle/index.test.tsx
+++ src/app/components/PackTitle/index.test.tsx
@@ -1,5 +1,43 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { PackTitle } from "@/app/components/PackTitle";
 
 describe("PackTitle Componet", function () {
-    it("", () => {});
+    // Test basic rendering and children
+    it("should render children correctly", function () {
+        const text = "Some text";
+        render(<PackTitle>{text}</PackTitle>);
+        expect(screen.getByText(text)).toBeInTheDocument();
+    });
+
+    // Test Tailwind class merging
+    it("should merge base styles with custom classes", function () {
+        const customClass = "bg-black";
+        render(<PackTitle className={customClass}>Some text</PackTitle>);
+
+        const packTitle = screen.getByRole("h3");
+
+        // Base styles verification
+        const baseStyles = [
+            "text-2xl",
+            "text-white",
+            "py-8",
+            "px-4",
+            "text-center",
+            "bg-[#f44336]",
+        ];
+
+        baseStyles.forEach((className) => {
+            expect(packTitle).toHaveClass(className);
+        });
+
+        // Custom class verification
+        expect(packTitle).toHaveClass(customClass);
+    });
+
+    // Test additional HTML attributes
+    it("should pass through other HTML attributes", function () {
+        render(<PackTitle lang="pt-br">Click me</PackTitle>);
+        expect(screen.getByRole("button")).toHaveAttribute("lang", "pt-br");
+    });
 });
