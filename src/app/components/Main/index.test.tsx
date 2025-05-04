b60a5d17887f536046922a79ea678053afdd37df
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-05-04 18:12:24 -0300

    test: main test

--- src/app/components/Main/index.test.tsx
+++ src/app/components/Main/index.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { Main } from "@/app/components/Main";
 
 describe("Main Componet", function () {
-    it("", () => {});
+    // Test basic rendering and children
+    it("should render children correctly", function () {
+        const text = "Some text";
+        render(<Main>{text}</Main>);
+        expect(screen.getByText(text)).toBeInTheDocument();
+    });
 });
